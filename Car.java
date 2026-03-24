98d41cd86eabadeaf477bb5e9fd4230c4d65ac88
Elijah Stenberg <elijah.stenberg.467@my.csun.edu>
2026-03-23 21:52:05 -0700

    Added fields and constructors

--- Car.java
+++ Car.java
@@ -1,1 +1,22 @@
-public class Car {}
+/**
+ * CSUN COMP 380/L - Spring 2026
+ * 
+ * @author EAM
+ */
+public class Car {
+    private String type;
+    private double price;
+    private boolean avail;
+    
+    public Car() {
+	this.type = "N/A";
+	this.price = 0;
+	this.availability = false;
+    }
+    
+    public Car(String type, double price, boolean avail) {
+	this.type = type;
+	this.price = price;
+	this.avail = avail;
+    }
+}
