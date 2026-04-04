96cb6e83e3aa35d92b5fc04503444ab25a8d1b51
mc802595 <muhtarim-afnan.chowdhury.442@my.csun.edu>
2026-04-03 19:10:40 -0700

    Updated EmailNotification.java

--- EmailNotification.java
+++ EmailNotification.java
@@ -3,30 +3,51 @@
  *
  * @AUTHOR EAM
  */
-PUBLIC CLASS EMAILNOTIFICATION {
-    PRIVATE STRING RECIPIENTEMAIL;
-    PRIVATE STRING SUBJECT;
-    PRIVATE STRING BODY;
+public class EmailNotification {
+    private String recipientEmail;
+    private String subject;
+    private String body;
 
-    // DEFAULT CONSTRUCTOR
-    PUBLIC EMAILNOTIFICATION() {
-        THIS.RECIPIENTEMAIL = "N/A";
-        THIS.SUBJECT = "N/A";
-        THIS.BODY = "N/A";
+    // default constructor
+    public EmailNotification() {
+        this.recipientEmail = "N/A";
+        this.subject = "N/A";
+        this.body = "N/A";
     }
 
-    // COMPLETE CONSTRUCTOR
-    PUBLIC EMAILNOTIFICATION(STRING RECIPIENTEMAIL, String subject, String body) {
+    // complete constructor
+    public EmailNotification(String recipientEmail, String subject, String body) {
         this.recipientEmail = recipientEmail;
         this.subject = subject;
         this.body = body;
     }
 
     public void formatEmail(Booking booking, Customer customer) {
+        if (booking == null || customer == null) return;
+
+        this.recipientEmail = customer.getEmail();
+        this.subject = "Booking Confirmation";
+
+        this.body = "Hello " + customer.getFirstName() + ",\n\n"
+                + "Your booking has been confirmed.\n"
+                + "Booking ID: " + booking.getBookingID() + "\n"
+                + "Car ID: " + booking.getCar().getCarID() + "\n"
+                + "Start Date: " + booking.getStartDate() + "\n"
+                + "End Date: " + booking.getEndDate() + "\n"
+                + "\nThank you for choosing our service!";
     }
 
     public boolean sendConfirmation(String email) {
-        return false;
+        if (email == null || email.equals("N/A")) {
+            return false;
+        }
+
+        // simulate sending email
+        System.out.println("Sending email to: " + email);
+        System.out.println("Subject: " + subject);
+        System.out.println("Body:\n" + body);
+
+        return true;
     }
 
     public String getBody() {
